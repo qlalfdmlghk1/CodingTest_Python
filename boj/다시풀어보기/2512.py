eb82400b9a34f3935549d04b26106755b43a5aa5
CHOI <cws98tiger@naver.com>
2024-12-13 09:27:25 +0900

    241212 업로드

--- boj/다시풀어보기/2512.py
+++ boj/다시풀어보기/2512.py
@@ -1,22 +1,27 @@
 n = int(input())
-money = list(map(int, input().split()))
-m = int(input())
+budget = list(map(int, input().split()))
+maxi = int(input())
 
-result = 0  # 출력할 최대 예산
-start, end = 1, max(money)
+budget.sort()
+left = 1
+right = budget[-1]
 
-while start <= end :
-    mid = (start + end) // 2  # 설정할 상한값
-    total = 0  # 예산의 합
-    for i in money :
-        if i > mid :        # 상한값보다 요청된 예산이 크다면
-            total += mid    # 상한값 배정
-        else :              # 상한값보다 요청된 예산이 작다면
-            total += i      # 예산 모두 배정
+answer = 0  # 최적의 상한값 저장
 
-    if total <= m :      # 계산한 값이 예산 총액보다 작다면
-        result = mid
-        start = mid + 1  # 시작값을 mid+1 로 변경
-    else :               # 계산한 값이 예산 총액보다 크다면
-        end = mid - 1    # 종료값을 mid-1로 변경
-print(result)
+while left <= right:  # 범위 조건 수정
+    mid = (left + right) // 2
+    total = 0
+
+    for b in budget:
+        if b > mid:
+            total += mid
+        else:
+            total += b
+
+    if total <= maxi:       # 총 예산이 허용 금액보다 작거나 같다면
+        answer = mid        # 가능한 상한값 업데이트  ->  여기가 키 포인트네
+        left = mid + 1      # 더 높은 값을 탐색
+    else:                   # 총 예산이 허용 금액보다 크다면
+        right = mid - 1     # 더 낮은 값을 탐색
+
+print(answer)               # 최적의 상한값 출력
