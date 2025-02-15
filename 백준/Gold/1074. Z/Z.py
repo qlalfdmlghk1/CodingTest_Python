df0fa7281dd9e1d7daa2090bb5f121d054bd1dee
WonSeo <121783811+qlalfdmlghk1@users.noreply.github.com>
2025-02-16 01:21:59 +0900

    [Gold V] Title: Z, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,27 +1,31 @@
 import sys
+# sys.setrecursionlimit(10^6)
 input = sys.stdin.readline
 
-n, r, c = map(int, input().split())  # n: 몇 승, r: 행, c: 열
+n,r,c = map(int,input().split()) # n : 몇 승, r : 행, c : 열
 
-def z(n, r, c):
-    result = 0
-    size = 2 ** n  # 전체 크기 (한 변의 길이)
-    
-    while size > 1:
-        size //= 2  # 4개의 영역으로 나누기 위한 크기 조정
-        if r < size and c < size:  # 1사분면 (좌상단)
-            pass  # 변화 없음
-        elif r < size and c >= size:  # 2사분면 (우상단)
-            result += size * size
-            c -= size  # 현재 위치를 2사분면의 좌상단 기준으로 이동
-        elif r >= size and c < size:  # 3사분면 (좌하단)
-            result += size * size * 2
-            r -= size  # 현재 위치를 3사분면의 좌상단 기준으로 이동
-        else:  # 4사분면 (우하단)
-            result += size * size * 3
-            r -= size
-            c -= size
+def z(cur_n, cur_r, cur_c, result) :
+    while cur_n > 1 :
+        cur_n = cur_n // 2
+        # 1영역
+        if cur_r < cur_n and cur_c < cur_n :
+            continue
 
+        # 2영역
+        elif cur_r < cur_n  and cur_c >= cur_n :
+            result += cur_n ** 2 * 1
+            cur_c -= cur_n
+
+        # 3영억
+        elif cur_r >= cur_n and cur_c < cur_n :
+            result += cur_n ** 2 * 2
+            cur_r -= cur_n
+
+        # 4영역
+        elif cur_r >= cur_n  and cur_c >= cur_n :
+            result += cur_n ** 2 * 3
+            cur_r -= cur_n
+            cur_c -= cur_n
     print(result)
 
-z(n, r, c)
+z(2**n,r,c,0)
