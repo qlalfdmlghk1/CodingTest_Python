5f010c9b66ad3ca090f268583d0cad87eecc2725
WonSeo <121783811+qlalfdmlghk1@users.noreply.github.com>
2025-03-19 00:48:00 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -0,0 +1,31 @@
+def solution(n, w, num):
+    target_width = num % w   # 0
+    target_hight = num // w + 1  # 2
+    if target_width == 0 :
+        target_hight -= 1
+
+    add_width = n % w  # 1
+    total_hight = n // w
+
+    layer = [total_hight for _ in range(w)]
+    if add_width > 0 :
+        if total_hight % 2 == 0 :
+            idx = 0
+            for i in range(add_width) :
+                layer[idx] += 1
+                idx += 1
+        else :
+            idx = w - 1
+            for i in range(add_width):
+                layer[idx] += 1
+                idx -= 1
+
+    if target_hight % 2 == 0 :
+        if target_width == 0 :
+            target_width = w
+        target_idx = w - target_width
+    else :
+        if target_width == 0 :
+            target_width = w
+        target_idx = target_width - 1
+    return layer[target_idx] - target_hight + 1
