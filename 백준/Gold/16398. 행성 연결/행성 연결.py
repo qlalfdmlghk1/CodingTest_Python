3695c35db5e9db26545de884f57c3fcc64ee0f57
WonSeo <121783811+qlalfdmlghk1@users.noreply.github.com>
2025-05-09 01:00:22 +0900

    [Gold IV] Title: 행성 연결, Time: 1812 ms, Memory: 178436 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -0,0 +1,37 @@
+n = int(input())
+
+parents = [0] * (n+1)
+for i in range(1,n+1) :
+    parents[i] = i
+
+graph = []
+for _ in range(n) :
+    graph.append(list(map(int,input().split())))
+
+def findSet(parents,a) :
+    if parents[a] != a :
+        parents[a] = findSet(parents,parents[a])
+    return parents[a]
+
+def union(parents,a,b) :
+    aRoot = findSet(parents,a)
+    bRoot = findSet(parents,b)
+    if aRoot < bRoot :
+        parents[bRoot] = aRoot
+    else :
+        parents[aRoot] = bRoot
+
+edges = []
+for i in range(n-1) :
+    for j in range(i+1,n) :
+        edges.append((i,j,graph[i][j]))
+        edges.append((j,i, graph[j][i]))
+edges.sort(key=lambda x:x[2])
+
+result = 0
+for edge in edges :
+    a,b,cost = edge
+    if findSet(parents,a) != findSet(parents,b) :
+        union(parents,a,b)
+        result += cost
+print(result)
