f4a97ef6abdf867aa0030b03740c7e3362e9cffc
WonSeo <121783811+qlalfdmlghk1@users.noreply.github.com>
2025-06-11 01:37:38 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 2664 ms, Memory: 51144 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,16 +1,10 @@
 import sys
 sys.setrecursionlimit(100000)
-v,e = map(int,input().split())
+v,e = map(int,input().split())  # 정점, 간선
 
-parents = [0] * (v+1)
-for i in range(1,v+1) :
-    parents[i] = i
-
+parents = [i for i in range(v+1)]
 edges = []
-for _ in range(e) :
-    a,b,c = map(int,input().split())
-    edges.append((a,b,c))
-edges.sort(key=lambda x:x[2])
+result = 0
 
 def findSet(parents,a) :
     if parents[a] != a :
@@ -20,15 +14,19 @@ def findSet(parents,a) :
 def union(parents,a,b) :
     aRoot = findSet(parents,a)
     bRoot = findSet(parents,b)
-    if aRoot < bRoot :
+    if aRoot <= bRoot :
         parents[bRoot] = aRoot
     else :
         parents[aRoot] = bRoot
 
-result = 0
+for _ in range(e) :
+    a,b,cost = map(int,input().split())
+    edges.append((a,b,cost))
+edges.sort(key=lambda x : x[2])
+
 for edge in edges :
-    a,b,c = edge
+    a,b,cost = edge
     if findSet(parents,a) != findSet(parents,b) :
         union(parents,a,b)
-        result += c
+        result += cost
 print(result)
