78b07ce9ef91abb0002e7dd5a9c6cd7374a3a140
WonSeo <121783811+qlalfdmlghk1@users.noreply.github.com>
2025-08-30 23:51:01 +0900

    [Silver II] Title: DNA 비밀번호, Time: 580 ms, Memory: 32852 KB -BaekjoonHub

---
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+s, p = map(int, input().split())
+dna = input()
+arr = list(map(int, input().split()))
+lp, rp = 0, p-1
+cnt = 0
+
+password = dna[lp:rp]
+for j in password:
+    if j == 'A': arr[0] -= 1
+    elif j == 'C': arr[1] -= 1
+    elif j == 'G': arr[2] -= 1
+    elif j == 'T': arr[3] -= 1
+
+
+while rp <= s-1 :
+    if dna[rp] == 'A': arr[0] -= 1
+    elif dna[rp] == 'C': arr[1] -= 1
+    elif dna[rp] == 'G': arr[2] -= 1
+    elif dna[rp] == 'T': arr[3] -= 1
+
+    cnt += 1
+    for j in arr :
+        if j > 0 :
+            cnt -= 1
+            break
+
+    if dna[lp] == 'A': arr[0] += 1
+    elif dna[lp] == 'C': arr[1] += 1
+    elif dna[lp] == 'G': arr[2] += 1
+    elif dna[lp] == 'T': arr[3] += 1
+
+
+    lp += 1
+    rp += 1
+
+print(cnt)
