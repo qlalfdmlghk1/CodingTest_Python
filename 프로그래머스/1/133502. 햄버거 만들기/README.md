491be496b4cb6a0aabfef1019cb21236dc30c8fd
WonSeo <121783811+qlalfdmlghk1@users.noreply.github.com>
2025-11-22 19:15:28 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 9.16 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 22일 19:08:11
+2025년 11월 22일 19:15:26
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,10 +1,14 @@
 def solution(ingredient):
-    stack = []
     answer = 0
-    for i in ingredient :
-        stack.append(i)
-        if stack[-4:] == [1,2,3,1] :
-            answer += 1
-            for j in range(4) :
+    stack = []
+    for ingre in ingredient :
+        stack.append(ingre)
+        if ingre == 1 and len(stack) >= 4 :  # 1이면 앞의 4개 확인
+            if stack[-4:] == [1, 2, 3, 1] :
                 stack.pop()
+                stack.pop()
+                stack.pop()
+                stack.pop()
+                answer += 1
+
     return answer
